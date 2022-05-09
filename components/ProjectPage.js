8d8b5d6a1be7806fd68abf8344217db61785548b
HittuDesai <hitarth.desai@mail.utoronto.ca>
2022-05-09 18:09:25 +0530

    Added json data for all projects and made project cards with dropdown animation for description for every single project

--- components/ProjectPage.js
+++ components/ProjectPage.js
@@ -1,6 +1,19 @@
+import ProjectCard from './ProjectCard'
+import projects from '../data/projects';
+
 function ProjectPage () {
     return (
-        <></>
+        <section id="project">
+            <div id="carousel">
+                {
+                    projects.map(project => {
+                        return (
+                            <ProjectCard key={project["name"]} project={project}/>
+                        );
+                    })
+                }
+            </div>
+        </section>
     );
 }
 
--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,8 @@
 import '../styles/globals.css'
 import '../styles/header.css'
 import '../styles/homepage.css'
+import '../styles/projectspage.css'
+import '../styles/projectcard.css'
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
