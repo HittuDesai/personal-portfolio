9c3b5e88e86adeb487077799287040a034416f8c
HittuDesai <hitarth.desai@mail.utoronto.ca>
2022-05-11 12:27:40 +0530

    Fixed issues with use of anchor tags in Header.js file

--- components/Header.js
+++ components/Header.js
@@ -29,9 +29,18 @@ function Header () {
             </div>
             <nav onMouseOver={handleMouseOver} onMouseLeave={handleMouseLeave}>
                 <ul>
-                    <li><a href="/">home</a></li>
-                    <li><a href="/">projects</a></li>
-                    <li><a href="/">contact</a></li>
+                    <li onClick={() => {
+                        var elem = document.getElementById("home");
+                        elem.scrollIntoView({"behaviour": "smooth",});
+                    }}>home</li>
+                    <li onClick={() => {
+                        var elem = document.getElementById("project");
+                        elem.scrollIntoView({"behaviour": "smooth",});
+                    }}>projects</li>
+                    <li onClick={() => {
+                        var elem = document.getElementById("contact");
+                        elem.scrollIntoView({"behaviour": "smooth",});
+                    }}>contact</li>
                 </ul>
             </nav>
         </header>
